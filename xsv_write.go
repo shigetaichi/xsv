cd8748b4e6fabbedf0d3d70750ed58d5e2f221cb
shigetaichi <shigetaichi1one@gmail.com>
2023-08-14 12:43:29 +0900

    tmp

--- xsv_write.go
+++ xsv_write.go
@@ -4,23 +4,26 @@ import (
 	"bytes"
 	"encoding/csv"
 	"os"
+	"reflect"
+	"slices"
 )
 
 type XsvWrite[T any] struct {
 	TagName             string //key in the struct field's tag to scan
 	TagSeparator        string //separator string for multiple csv tags in struct fields
 	OmitHeaders         bool
-	selectedColumnIndex []int        // slice indexes of selected columns
+	selectedColumns []string        // slice indexes of selected columns
 	columnSorter        ColumnSorter // TODO: describe in comment
 	nameNormalizer      Normalizer
 }
+type ColumnSorter = func(row []string) []string
 
 func NewXsvWrite[T any]() XsvWrite[T] {
 	return XsvWrite[T]{
 		TagName:             "csv",
 		TagSeparator:        ",",
 		OmitHeaders:         false,
-		selectedColumnIndex: make([]int, 0),
+		selectedColumns: make([]string, 0),
 		columnSorter: func(row []string) []string {
 			return row
 		},
@@ -28,7 +31,18 @@ func NewXsvWrite[T any]() XsvWrite[T] {
 	}
 }
 
-type ColumnSorter = func(row []string) []string
+func (x *XsvWrite[T]) getIndexesOfSelectedColumns() (columnFieldsIndexes []int) {
+	var writeDataType T
+	field := reflect.TypeOf(writeDataType)
+	var fieldNames []string
+	for i := 0; i < field.NumField(); i++ { // TODO:もっといいやり方あるはず。
+		fieldNames = append(fieldNames, field.Field(i).Tag.Get(x.TagName))
+	}
+	for _, column := range x.selectedColumns {
+		columnFieldsIndexes = append(columnFieldsIndexes, slices.Index(fieldNames, column))
+	}
+	return columnFieldsIndexes
+}
 
 func (x *XsvWrite[T]) SetWriter(writer *csv.Writer) (xw *XsvWriter[T]) {
 	xw = NewXsvWriter(*x)
